f77245cae84b83cc3812c09402f2f06e358a1cc7
ansonkwokth <ansonwos@gmail.com>
2022-03-16 17:08:16 +0800

    Reconstruction for jjl events only.

--- Reconstructor.C
+++ Reconstructor.C
@@ -21,6 +21,7 @@ iFinalStates FindFinalStatesIndex(TClonesArray* branchTrack, TClonesArray* branc
     Jet* jet1;
     Jet* jet2;
 
+    // required 2 jets event
     if (nJets != 2) return iFinalStatesIndexes;
 
     Int_t iLep = 99999;
@@ -28,38 +29,43 @@ iFinalStates FindFinalStatesIndex(TClonesArray* branchTrack, TClonesArray* branc
     Int_t nMu = 0;
     for (Int_t it = 0; it < nTracks; it++) {
         track1 = (Track*)branchTrack->At(it);
+        // muon
         if (abs(track1->PID) == 13) {
             nMu += 1;
             iLep = it;
+            // electron
         } else if (abs(track1->PID) == 11) {
             nEle += 1;
             iLep = it;
         }
     }
+    // required only one lepton, either muon or electron
     if (nMu + nEle != 1) return iFinalStatesIndexes;
 
-    TLorentzVector j1, j2, lep, N;
     jet1 = (Jet*)branchJet->At(0);
     jet2 = (Jet*)branchJet->At(1);
     track1 = (Track*)branchTrack->At(iLep);
-    j1.SetPtEtaPhiM(jet1->PT, jet1->Eta, jet1->Phi, jet1->Mass);
-    j2.SetPtEtaPhiM(jet2->PT, jet2->Eta, jet2->Phi, jet2->Mass);
 
+    // set the lepton mass, check if it is muon or electron
     Float_t mLep;
     if (nMu == 1) mLep = 0.1056583745;
-    if (nEle == 1) mLep = 0.51099894615109989461;
+    if (nEle == 1) mLep = 0.00051099894615109989461;
 
+    TLorentzVector j1, j2, lep;
+    j1.SetPtEtaPhiM(jet1->PT, jet1->Eta, jet1->Phi, jet1->Mass);
+    j2.SetPtEtaPhiM(jet2->PT, jet2->Eta, jet2->Phi, jet2->Mass);
     lep.SetPtEtaPhiM(track1->PT, track1->Eta, track1->Phi, mLep);
 
-    if (not(lep.Px() * j1.Px() + lep.Py() * j1.Py() + lep.Pz() * j1.Pz() >= 0)) return iFinalStatesIndexes;
-    if (not(lep.Px() * j2.Px() + lep.Py() * j2.Py() + lep.Pz() * j2.Pz() >= 0)) return iFinalStatesIndexes;
-    if (not(j2.Px() * j1.Px() + j2.Py() * j1.Py() + j2.Pz() * j1.Pz() >= 0)) return iFinalStatesIndexes;
-
-    iFinalStatesIndexes.iLep = iLep;
-    iFinalStatesIndexes.iJet1 = 0;
-    iFinalStatesIndexes.iJet2 = 0;
-    iFinalStatesIndexes.mLep = mLep;
-    iFinalStatesIndexes.foundAll = 1;
+    iFinalStatesIndexes.iLep = iLep;  // save lepton index
+    if (jet1->PT > jet2->PT) {
+        iFinalStatesIndexes.iJet1 = 0;  // save jet index
+        iFinalStatesIndexes.iJet2 = 1;  // save jet index
+    } else {
+        iFinalStatesIndexes.iJet1 = 1;  // save jet index
+        iFinalStatesIndexes.iJet2 = 0;  // save jet index
+    }
+    iFinalStatesIndexes.mLep = mLep;   // save lepton mass
+    iFinalStatesIndexes.foundAll = 1;  // flag to process
 
     return iFinalStatesIndexes;
 }
