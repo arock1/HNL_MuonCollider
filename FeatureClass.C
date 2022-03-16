227104ee5f38d415aed560a495b44b8b68429cee
ansonkwokth <ansonwos@gmail.com>
2022-03-16 17:07:42 +0800

    added new features for comparison.

--- FeatureClass.C
+++ FeatureClass.C
@@ -1,8 +1,28 @@
 class Features {
 public:
     Int_t iEvt;
-    Float_t ptMu;
-    Float_t mjjl;
+    Float_t ptLep;
+    Float_t etaLep;
+    Float_t phiLep;
+    Float_t ptJet1;
+    Float_t etaJet1;
+    Float_t phiJet1;
+    Float_t ptJet2;
+    Float_t etaJet2;
+    Float_t phiJet2;
+    Float_t DeltaRjj;
+    Float_t DeltaRjjl;
+    Float_t mN;
 
-    Float_t ptMuTrue;
+    Float_t ptLepTrue;
+    Float_t etaLepTrue;
+    Float_t phiLepTrue;
+    Float_t ptJet1True;
+    Float_t etaJet1True;
+    Float_t phiJet1True;
+    Float_t ptJet2True;
+    Float_t etaJet2True;
+    Float_t phiJet2True;
+    Float_t DeltaRjjTrue;
+    Float_t DeltaRjjlTrue;
 };
