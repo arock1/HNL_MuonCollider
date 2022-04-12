32de94f8c79042cc30c07ff764e4b2122ae8e6ec
ansonkwokth <ansonwos@gmail.com>
2022-04-12 16:28:01 +0800

    added geo related function.

---
+++ reconstruction/Geometry.C
@@ -0,0 +1,13 @@
+
+#include "TMath.h"
+
+Float_t deltaPhi(Float_t phi1, Float_t phi2) {
+    Float_t substr = abs(phi1 - phi2);
+    while (true) {
+        if (substr < TMath::Pi()) {
+            return substr;
+        } else {
+            substr -= TMath::Pi();
+        }
+    }
+}
