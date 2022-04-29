06b5ee02e0b25a0a99e6fea573c356455aacf8d2
ansonkwokth <ansonwos@gmail.com>
2022-04-29 16:42:07 +0800

    fixed bug

--- reconstruction/Geometry.C
+++ reconstruction/Geometry.C
@@ -5,11 +5,9 @@ Float_t deltaPhi(Float_t phi1, Float_t phi2) {
     Float_t substr = abs(phi1 - phi2);
     if (substr <= TMath::Pi()) {
         return substr;
-    } else if (substr > TMath::Pi() && substr <= 2 * TMath::Pi()) {
-        return 2 * TMath::Pi() - substr;
     } else {
-        cout << " =====================================================\nGeometry deltaPhi errors: "
-             << "\n";
+        //(substr > TMath::Pi() && substr <= 2 * TMath::Pi()) {
+        return 2 * TMath::Pi() - substr;
     }
 }
 
